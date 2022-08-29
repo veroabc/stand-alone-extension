1efe68eafe75d0d5bf97fecdf16e79d7878bae9e
veroabc <91472809+veroabc@users.noreply.github.com>
2022-08-29 16:55:24 +0200

    Create datei.js
    Functions to calculate customer requests. Actually without Unit Tests

---
+++ stand-alone extention/datei.js
@@ -0,0 +1,9 @@
+function hour() {
+    num1 = document.getElementById("firstNumber").value;
+    document.getElementById("result").innerHTML = num1 * 0.5;
+}
+
+function distance() {
+    num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result2").innerHTML = num2 * 1.50;
+}
